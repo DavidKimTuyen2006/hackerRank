fc3ba039d4967a7862d6fba80d2012c64d5c370f
DucManhHuynh <manhhdleo@gmail.com>
2025-11-22 22:48:34 -0600

    Update TimeConversion.py

--- TimeConversion.py
+++ TimeConversion.py
@@ -1,17 +1,18 @@
 def timeConversion(s):
     day_time = s[-2]
     time = s[0:2]
-    
-    if day_time == 'P' and time == "12":
-        return s[0:8]
-    elif day_time == 'A' and time == "12":
-        time = "00"
-        return time + s[2:8]
         
-    elif day_time == 'P':
-        time = int(time) + 12
-        time = str(time) 
-        return time + s[2:8]
+    if day_time == 'P':
+        if time == "12":
+            return s[0:8]
+        else:
+            time = int(time) + 12
+            time = str(time) 
+            return time + s[2:8]
          
     elif day_time == 'A':
-        return time + s[2:8]
+        if time == "12":
+            time = "00"
+            return time + s[2:8]
+        else:
+            return time + s[2:8]
