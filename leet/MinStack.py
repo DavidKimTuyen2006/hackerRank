6bd614c47ba748365c5448f6ebdc991fca438083
DavidKimTuyen2006 <takien1542006@gmail.com>
2026-02-03 23:02:14 +0700

    3/2/2026 kiệt nó khôn quá

---
+++ leet/MinStack.py
@@ -0,0 +1,32 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append([val, val])
+        else:
+            tmp_min = self.stack[-1][1]
+            self.stack.append([val, min(val, tmp_min)])
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1][0]
+        return None 
+
+    def getMin(self) -> int:
+        if self.stack:
+            return self.stack[-1][1]
+        return None 
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
